862e98a66e015a3df2de4f6d0c2cf025f2c8399e
filmartfm <filmartfm@gmail.com>
2025-09-07 05:11:01 +0000

    ssa

---
+++ node-scripts/operations/node/index.mjs
@@ -0,0 +1,36 @@
+/* Operaciones de conjunto */
+
+class Node {
+
+	#nodeLinks;
+	#nodeValue;
+	constructor() {
+		
+	}
+
+	_addLink(node) {}
+	_remLink(node) {}
+	_resolveLink(linkKey) {}
+	_applyMatrix(nodeMatrix) {}
+
+}
+
+class NodeLink {
+
+	constructor(node) {
+
+	}
+
+}
+
+class NodeMatrix {
+
+	constructor() {
+
+	}
+
+	_apply(node) {}
+
+}
+
+export { Node, NodeMatrix }
